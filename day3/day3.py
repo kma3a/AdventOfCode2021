acb68c4daab5213397d329e810035466f75bb3f0
Kelsey Ando <kelsey.ando@detroitlabs.com>
2021-12-03 15:28:05 -0500

    making it so I can send in the filename to open and that way

--- day3/day3.py
+++ day3/day3.py
@@ -1,3 +1,6 @@
+import sys
+file_name = f'{sys.argv[1]}.txt'
+
 def getZeroCount(data, position):
     count=0
     for binary in data:
@@ -5,66 +8,66 @@ def getZeroCount(data, position):
             count+=1
     return count
 
-# part 1
-# with open('input.txt') as f:
-#     data = f.read().splitlines()
-#     gamma=[]
-#     epsilon=[]
-#     index=0
-#     while index < len(data[0]):
-#         if getZeroCount(data, index) > len(data)/2:
-#             gamma.append('0')
-#             epsilon.append('1')
-#         else:
-#             gamma.append('1')
-#             epsilon.append('0')
-#         index+=1
-#     gamma_num = int("".join(gamma),2) 
-#     epsilon_num = int("".join(epsilon),2) 
-#     print(gamma_num*epsilon_num)
+### part 1
+with open(file_name) as f:
+    data = f.read().splitlines()
+    gamma=[]
+    epsilon=[]
+    index=0
+    while index < len(data[0]):
+        if getZeroCount(data, index) > len(data)/2:
+            gamma.append('0')
+            epsilon.append('1')
+        else:
+            gamma.append('1')
+            epsilon.append('0')
+        index+=1
+    gamma_num = int("".join(gamma),2) 
+    epsilon_num = int("".join(epsilon),2) 
+    print(gamma_num*epsilon_num)
 
-#     f.close()
+    f.close()
 
 
 ## part 2
 
 
-def removeData(data, remove, index):
-    array =[]
-    for binary in data:
-        if binary[index] != remove:
-            array.append(binary)
-    return array
+# def removeData(data, remove, index):
+#     array =[]
+#     for binary in data:
+#         if binary[index] != remove:
+#             array.append(binary)
+#     return array
 
-def getOxygen(data):
-    index=0
-    while len(data) > 1:
-        remove = '0'
-        zero_count = getZeroCount(data, index)
-        if zero_count > len(data)/2:
-            remove = '1'
-        data = removeData(data, remove, index)
-        index+=1
-    return int(data[0],2)
+# def getOxygen(data):
+#     index=0
+#     while len(data) > 1:
+#         remove = '0'
+#         zero_count = getZeroCount(data, index)
+#         if zero_count > len(data)/2:
+#             remove = '1'
+#         data = removeData(data, remove, index)
+#         index+=1
+#     return int(data[0],2)
 
-def getCO(data):
-    index=0
-    while len(data) > 1:
-        remove = '1'
-        zero_count = getZeroCount(data, index)
-        if zero_count > len(data)/2:
-            remove = '0'
-        data = removeData(data, remove, index)
-        index+=1
-    return int(data[0],2)
-
-
+# def getCO(data):
+#     index=0
+#     while len(data) > 1:
+#         remove = '1'
+#         zero_count = getZeroCount(data, index)
+#         if zero_count > len(data)/2:
+#             remove = '0'
+#         data = removeData(data, remove, index)
+#         index+=1
+#     return int(data[0],2)
 
 
 
-with open('test.txt') as f:
-    data = f.read().splitlines()
-    oxygen = getOxygen(data)
-    co = getCO(data)
-    print(oxygen*co)
-    f.close()
+
+
+# with open(file_name) as f:
+#     data = f.read().splitlines()
+#     oxygen = getOxygen(data)
+#     co = getCO(data)
+#     print(oxygen*co)
+#     f.close()
