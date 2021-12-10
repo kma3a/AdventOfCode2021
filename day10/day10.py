ba57269ba458d886c6b8ae163cb2377cd824e945
Kelsey Ando <kelsey.ando@detroitlabs.com>
2021-12-10 12:34:31 -0500

    adding what I have for the second part of day 10

--- day10/day10.py
+++ day10/day10.py
@@ -12,12 +12,13 @@ count = {
     '>':0,
 }
 
-points = {
-    ')':3,
-    ']':57,
-    '}':1197,
-    '>':25137,
-}
+## part 1 points
+# points = {
+#     ')':3,
+#     ']':57,
+#     '}':1197,
+#     '>':25137,
+# }
 
 def check_legal(line):
     finished = line
@@ -72,26 +73,62 @@ def get_illegal_character(line):
 # print(illegal_mark == ']')
 
 ### part 1
+# with open(file_name) as f:
+#     data = f.read().splitlines()
+#     for line in data:
+#         get_legal = check_legal(line)
+#         if len(get_legal) > 0:
+#             illegal_mark = get_illegal_character(get_legal)
+#             if illegal_mark == '':
+#                 continue
+#             value = count[illegal_mark] 
+#             count[illegal_mark] = value+1
+#     score = 0
+#     for key in points.keys():
+#         score+= points[key] * count[key]
+#     print(score)
+
+#     f.close()
+
+
+## part 1 points
+points = {
+    '(':1,
+    '[':2,
+    '{':3,
+    '<':4,
+}
+
+def count_marks(line):
+    score = 0
+    for mark in line[::-1]:
+        score = (score *5) + points[mark]
+    return score
+
+# test = '<{([{{}}[<[[[<>{}]]]>[]]'
+# illegal = check_legal(test)
+# test_score = count_marks(illegal)
+# print(test_score == 294)
+
+
+# ## part 2
 with open(file_name) as f:
+    scores = []
     data = f.read().splitlines()
     for line in data:
         get_legal = check_legal(line)
         if len(get_legal) > 0:
             illegal_mark = get_illegal_character(get_legal)
             if illegal_mark == '':
-                continue
-            value = count[illegal_mark] 
-            count[illegal_mark] = value+1
-    score = 0
-    for key in points.keys():
-        score+= points[key] * count[key]
-    print(score)
-
-    f.close()
+                scores.append(count_marks(get_legal))
+    scores.sort()
+    print(scores[math.floor(len(scores)/2)])
 
 
-# ## part 2
-# with open(file_name) as f:
-#     data = f.read().splitlines()
-#     f.close()
+    # score = 0
+    # for key in points.keys():
+    #     score+= points[key] * count[key]
+    # print(score)
+
+#     f.close())
 
