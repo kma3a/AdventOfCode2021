c4d5d08a078ab026dc99dc0d60e934923b72866f
Kelsey Ando <kelsey.ando@detroitlabs.com>
2021-12-10 12:40:13 -0500

    adding day 11

---
+++ day11/day11.py
@@ -0,0 +1,19 @@
+import sys
+import math
+check_argv = len(sys.argv)
+default = 'test'
+input = sys.argv[1] if check_argv>1 else default
+file_name = f'{input}.txt'
+
+### part 1
+with open(file_name) as f:
+    data = f.read().splitlines()
+    f.close()
+
+
+# ## part 2
+# with open(file_name) as f:
+#     scores = []
+#     data = f.read().splitlines()
+#     f.close())
+
